af86527379d00459395f813610d74435fa3a8601
Veera Prakash <veera.p0820@gmail.com>
2021-06-25 15:06:52 -0500

    feat: initial commit

---
+++ src/main/java/com/webapp/projects/firstwebapp/FirstWebappApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.projects.firstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/projects/firstwebapp/FirstWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.projects.firstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
