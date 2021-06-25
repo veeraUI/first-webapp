6fc7a18ff4f8e38e9ba7b02e9aa88daffec4f860
Veera Prakash <veera.p0820@gmail.com>
2021-06-25 16:46:47 -0500

    feat: creating table with entities

--- src/main/java/com/webapp/projects/firstwebapp/controller/Course.java
+++ src/main/java/com/webapp/projects/firstwebapp/controller/Course.java
@@ -2,17 +2,27 @@ package com.webapp.projects.firstwebapp.controller;
 
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Component
+@Entity(name = "CourseTable") // creates table name as COURSE_TABLE instead of COURSE
 public class Course{
+    @Id
+    @GeneratedValue
     private long id;
     private String name;
+    @Column(name = "course_desc") // creates columns name as course_desc instead of default desc
     private String desc;
+    private String author;
 
     public String getAuthor(){
         return author;
     }
 
-    private String author;
+
 
     public long getId(){
         return id;
