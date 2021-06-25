b8bdfbd3cf1cfeabf1519b1f97430c10101d45b7
Veera Prakash <veera.p0820@gmail.com>
2021-06-25 16:18:38 -0500

    feat: spring boot actuator

--- src/main/java/com/webapp/projects/firstwebapp/controller/CourseController.java
+++ src/main/java/com/webapp/projects/firstwebapp/controller/CourseController.java
@@ -8,10 +8,16 @@ import java.util.List;
 
 @RestController
 public class CourseController{
+    List<Course> courses=Arrays.asList(new Course(1, "java", "Object oriented programming", "veera"), new Course(2, "react", "Declarative Programming Language", "veera"));
     @GetMapping("/courses")
     public List<Course> getAllCourses() {
-        return Arrays.asList(new Course(1, "java", "Object oriented programming", "veera"));
+        return courses;
 
+    }
+
+    @GetMapping("/courses/1")
+    public Course getCourse() {
+        return courses.get(1);
     }
 
 }
