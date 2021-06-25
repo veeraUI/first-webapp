8e35a87e3821f1b3affb11dd1fe0b042779102c3
Veera Prakash <veera.p0820@gmail.com>
2021-06-25 17:17:34 -0500

    feat: get api with path variables

--- src/main/java/com/webapp/projects/firstwebapp/controller/CourseController.java
+++ src/main/java/com/webapp/projects/firstwebapp/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.webapp.projects.firstwebapp.controller;
 import com.webapp.projects.firstwebapp.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,9 +19,15 @@ public class CourseController{
 
     }
 
-    @GetMapping("/courses/1")
-    public Optional<Course> getCourse() {
-        return repository.findById(10001l);
+    @GetMapping("/courses/{id}")
+    public Course getCourse(@PathVariable long id) {
+        Optional<Course> courseById=repository.findById(id);
+
+
+        if(!courseById.isPresent()) {
+            throw new RuntimeException("Course not found with id " +  id);
+        }
+        return courseById.get();
     }
 
 }
